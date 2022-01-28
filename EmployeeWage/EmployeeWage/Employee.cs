d0154a57419e09cf0e2882adf906d8f63e835938
shubham <s.shrikhande1@gmail.com>
2022-01-28 16:00:43 +0530

    EmployeeWage Attendance

---
+++ EmployeeWage/EmployeeWage/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class Employee
+    {
+        int Present = 0;
+        public void Attendance()
+        {
+            Random random = new Random();
+            int result = random.Next(0, 2);
+            if (result == 0)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
