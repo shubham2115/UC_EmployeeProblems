01348e4978c882c29fad760ca2d23b479b73e4d1
Shubham <s.shrikhande1@gmail.com>
2022-02-04 23:55:49 +0530

    Ability to save the total wage for each company

--- EmployeeWage/EmployeeWage/Employee.cs
+++ EmployeeWage/EmployeeWage/Employee.cs
@@ -11,12 +11,27 @@ namespace EmployeeWage
 
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
-        public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+
+        private string company;
+        private int empRatePerHour;
+        private int numOfWorkingDays;
+        private int maxHoursPerMonth;
+        private int totalEmpWage;
+
+        public void empWageBuilderObject(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+
+        public void computeEmpWage()
         {
             
             int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
             
-            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
+            while (totalEmpHrs <= this.maxHoursPerMonth && totalWorkingDays < this.numOfWorkingDays)
             {
                 totalWorkingDays++;
                 Random random = new Random();
@@ -34,15 +49,19 @@ namespace EmployeeWage
                         break;
                 }
                 totalEmpHrs += empHrs;
-                Console.WriteLine("Day : " + totalWorkingDays + " Emp Hours : " + empHrs);
+                Console.WriteLine("Day:" + totalWorkingDays + " Emp Hours:" + empHrs);
             }
-            int totalEmpWage = totalEmpHrs * empRatePerHour;
-            Console.WriteLine("Total Emp Wage for company :" + company + " is " + totalEmpWage);
-            return totalEmpWage;
+            totalEmpWage = totalEmpHrs * this.empRatePerHour;
+            Console.WriteLine("Total Emp Wage For Company : " + company + " is " + totalEmpWage);
+        }
+
+        public string toString()
+        {
+            return "Total Emp Wage For Company : " + this.company + " is " + this.totalEmpWage;
         }
     }
-
-
-    
-    
 }
+    
+
+    
+    
