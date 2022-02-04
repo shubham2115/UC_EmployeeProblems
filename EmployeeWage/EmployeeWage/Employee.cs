4b9108ba3b4cfac0b155251b42c087e54d10bd06
Shubham <s.shrikhande1@gmail.com>
2022-02-04 21:49:21 +0530

    Employee wage for multiple company

--- EmployeeWage/EmployeeWage/Employee.cs
+++ EmployeeWage/EmployeeWage/Employee.cs
@@ -6,20 +6,17 @@ using System.Threading.Tasks;
 
 namespace EmployeeWage
 {
-    internal class Employee
+    public class Employee
     {
+
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 20;
-        public const int MAX_HRS_IN_MONTH = 100;
-
-        public static int computeEmpWage()
+        public static int computeEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
         {
-            //Variable
+            
             int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-            //Computation
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < MAX_HRS_IN_MONTH)
+            
+            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
             {
                 totalWorkingDays++;
                 Random random = new Random();
@@ -37,14 +34,15 @@ namespace EmployeeWage
                         break;
                 }
                 totalEmpHrs += empHrs;
-                Console.WriteLine("Day :" + totalWorkingDays + " Emp Wage : " + empHrs);
+                Console.WriteLine("Day : " + totalWorkingDays + " Emp Hours : " + empHrs);
             }
-            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total emp wage : " + totalEmpWage);
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Emp Wage for company :" + company + " is " + totalEmpWage);
             return totalEmpWage;
         }
-
-
     }
+
+
+    
     
 }
